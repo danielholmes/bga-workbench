e83522e35e9ed8ac896f0a030e1e91695ae187d0
Daniel Holmes <daniel@danielholmes.org>
2017-09-21 19:54:43 +1000

    update bin autoload

--- bin/bgawb.php
+++ bin/bgawb.php
@@ -1,6 +1,15 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+$autoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php'
+];
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        require_once($autoloadFile);
+        break;
+    }
+}
 
 use BGAWorkbench\Commands\BuildCommand;
 use BGAWorkbench\Commands\ValidateCommand;
